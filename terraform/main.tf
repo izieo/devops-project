8a90ca727162b7942ea9f04edb084fca5a7824a0
Izie Omoijiade <izieomoijiade@yahoo.com>
2025-06-25 19:22:59 +0100

    refractored code in terraform/main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -44,6 +44,14 @@ resource "azurerm_kubernetes_cluster" "aks" {
   }
 }
 
+#-------------------------
+# Grant AKS access to ACR
+#-------------------------
+resource "azurerm_role_assignment" "aks_acr_pull" {
+  scope                = azurerm_container_registry.acr.id
+  role_definition_name = "AcrPull"
+  principal_id         = azurerm_kubernetes_cluster.aks.identity[0].principal_id
+}
 
 #-------------------------
 # Output kubeconfig
